c71e0f62ac315b983e0b21c382d34982d7aeaa3f
mccann-b <brian@cedrus.digital>
2020-09-13 20:16:29 -0400

    got set up for silly math

---
+++ sillyMath/problem.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+
+// convert both numbers into arrays of strings
+
+// find which array is longer
+
+// starting with the longer array loop backward
+
+// while indexes exist and the two digits together
+// push the result into a results array
+
+
+// if the longer array still has digits just append those as is to the front of the result
+
+  return num1 + num2;
+  
+}
+
+
+module.exports = add;
